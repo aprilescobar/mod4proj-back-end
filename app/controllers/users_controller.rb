683171b34758b5cfaef10054cc65f7e602b886b0
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-17 07:42:02 -0400

    all about Nike

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, except: [:updated_at]
+     end 
+  
+    def show 
+        user = User.find(params[:id])
+        render json: user, except: [:updated_at]
+     end 
+  
+     def create
+        # let's make a user using the username and password from the params
+        user = User.new(
+          username: params[:username],
+          password: params[:password],
+        )
+    
+        if user.save
+        #   token = encode_token(user.id)
+          render json: {user: user}
+        else
+          render json: {errors: user.errors.full_messages}
+        end
+    
+      end
+
+  
+     def update
+        user = user.find_by(id: params[:id])
+        user.update(user_params)
+        render json: user, except: [:updated_at], status:201
+     end 
+  
+     def destroy 
+        user = user.find_by(id: params[:id])
+        user.destroy
+     end 
+  
+     private 
+     def user_params
+        params.require(:user).permit(:username, :password)
+     end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,104 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-mt1 = Top.create(brand: "Club Monaco", name: "Slim Denim Shirt", color: "Dark Wash", description: "For every day. The perfect denim button-down, precisely tailored for a slim fit. In 100% cotton with a versatile light blue wash.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1294830_lifestyle?$CMPDP$")
-mt2 = Top.create(brand: "Club Monaco", name: "Feel Good Sweater", color: "White", description: "Feeling fine. CM's newest crewneck sweater is gender neutral, made of 100% polyester from recycled bottles, and recyclable so you can look and feel good all at once.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1365791_lifestyle?$CMPDP$")
-mt3 = Top.create(brand: "Club Monaco", name: "Williams Long-Sleeve Crew", color: "Black", description: "A better casual T-shirt. In our softest cotton with long-sleeves for fall layering.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1346521_lifestyle?$CMPDP$")
+mt1 = Top.create(brand: "Nike", name: "Nike Pro", color: "White", description: "mens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ohvdtcsn8eyjdsqyybp7/pro-mens-long-sleeve-top-sDW22X.jpg")
+mt2 = Top.create(brand: "Nike", name: "Nike Pro", color: "Black", description: "mens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ezl7as1zmh4s0kbmkwcs/pro-mens-long-sleeve-top-sDW22X.jpg")
+mt3 = Top.create(brand: "Nike", name: "Nike Pro", color: "Grey", description: "mens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/l8frppueyjf2adx5adb3/pro-mens-long-sleeve-top-sDW22X.jpg")
 
-mb1 = Bottom.create(brand: "Club Monaco", name: "Super Slim Denim", color: "Black", description: "CM's slimmest denim. A close fit and classic styling bring a polished, streamlined update to the traditional blue jean.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1294901_lifestyle?$CMPDP$")
-mb2 = Bottom.create(brand: "Club Monaco", name: "Super Slim Denim", color: "Crescent Cream", description: "CM's slimmest denim. A close fit and classic styling bring a polished, streamlined update to the staple jean. In seasonal cream denim.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1371857_lifestyle?$CMPDP$")
-mb3 = Bottom.create(brand: "Club Monaco", name: "Super Slim Denim", color: "Grey", description: "CM's slimmest denim. A close fit and classic styling bring a polished, streamlined update to a grey denim jean.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1365780_alternate1?$CMPDP$")
+mt4 = Top.create(brand: "Nike", name: "Nike Therma Swoosh", color: "White", description: "mens", img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/pcmy5zqpd6m9jowte0mq/therma-swoosh-mens-pullover-training-hoodie-w0sL09.jpg")
+mt5 = Top.create(brand: "Nike", name: "Kyrie", color: "Black", description: "mens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/pz8at96yre3z2q80pgad/kyrie-mens-basketball-hoodie-XD1wmg.jpg")
+mt6 = Top.create(brand: "Nike", name: "Kyrie", color: "Grey", description: "mens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ph4nlefcp5ovmbxfgais/kyrie-mens-basketball-hoodie-XD1wmg.jpg")
 
-ms1 = Shoe.create(brand: "Red Wing", name: "Classic Moc", color: "Brown", description: "True heritage. First made by Red Wing in 1952, a pair of classic moc boots crafted in Navy Portage leather. From Red Wing, an American footwear brand known for its iconic boots and shoes crafted with premium leather for a lifetime of wear.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1352700_lifestyle?$CMPDP$")
-ms2 = Shoe.create(brand: "Club Monaco", name: "Leather Sneaker", color: "White", description: "The Club Monaco sneaker. A modern, clean-lined design in smooth leather with refined detailing. Made to be worn everyday, with everything.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1297304_lifestyle?$CMPDP$")
-ms3 = Shoe.create(brand: "Red Wing ", name: "Weekender Chukka", color: "Hawthorne", description: "Off the clock. Classic chukka boots crafted in Hawthorne Muleskinner leather using a reverse-suede technique for a textured feel. From Red Wing, an American footwear brand known for its iconic boots and shoes crafted with premium leather for a lifetime of wear.", img_url: "https://www.rlmedia.io/is/image/PoloGSI/s7-1311103_lifestyle?$CMPDP$")
+# mt7 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# mt8 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# mt9 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
 
-wt1 = Top.create(brand: "Ten by Babaton", name: "Ace Bodysuit", color: "Black", description: "This is a thong-cut, bodysuit blouse with a tie-front and menswear-inspired lapel. It's made from luxe satin.", img_url: "https://aritzia.scene7.com/is/image/Aritzia/large/f19_01_a02_75452_12269_on_a.jpg")
-wt2 = Top.create(brand: "Free People", name: "Gold Coast Bodysuit", color: "Rose Gold", description: "Slinky crochet bodysuit featured in a semi-sheer design with a V-neckline and a subtle metallic sparkle.", img_url: "https://s7d5.scene7.com/is/image/FreePeople/50777697_065_c?$a15-pdp-detail-shot$&hei=900&qlt=80&fit=constrain") 
-wt3 = Top.create(brand: "Nike", name: "Nike Sportswear Tech Fleece Pullover Hoodie", color: "Black/Gray", description: "The Nike Sportswear Tech Fleece Pullover Hoodie gives you the soft warmth of fleece with classic chevron lines and a modern silhouette.", img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoWaz4doxBhHjot5rZ3TVlTPEuFYC5swrNesYrp9h-YHf_Elv7&usqp=CAU")  
+# mt10 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# mt11 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mt12 = Top.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
 
-wb1 = Bottom.create(brand: "Free People", name: "CRVY High-Rise Lace-Up Skinny Jeans", color: "Snow White", description: "Introducing: The CRVY Collection, denim redefined with your shape in mind. These super-high-rise skinny jeans feature front lace-up detailing.", img_url: "https://s7d5.scene7.com/is/image/FreePeople/48602916_010_a?$a15-pdp-detail-shot$&hei=900&qlt=80&fit=constrain") 
-wb2 = Bottom.create(brand: "Free People", name: "CRVY Mercury Skinny", color: "Seattle Purple", description: "Super high-rise skinny jeans featuring back-zip detail and exposed seaming throughout with ankle zip feature in a soft stretch denim fabrication.", img_url: "https://s7d5.scene7.com/is/image/FreePeople/53388526_050_a?$a15-pdp-detail-shot$&hei=900&qlt=80&fit=constrain")
-wb3 = Bottom.create(brand: "Lululemon", name: "Align Pant 28", color: "Diamond Dye Pitch Grey Graphite Grey", description: "Buttery-Soft, sweat-wicking, lightweight material. These pants are designed to minimize distractions and maximize comfort as you flow through yoga.", img_url: "https://images.lululemon.com/is/image/lululemon/LW5COUS_043990_1?$image_carousel$&wid=1440&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72") 
+# mb1 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# mb2 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# mb3 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
 
-ws1 = Shoe.create(brand: "Nike", name: "Air Max 200 sneakers", color: "Soft Pink", description: "Lace-up fastening; mesh panels for added breathability. Units contain pressurized air that compresses on impact for lightweight, durable cushioning.”, img_url: “https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ95nSZnx_VGO5lnO5MG_Kn9PJ0FXQl4tmlsH9UaBybGDSUJTdl&usqp=CAU")   
-ws2 = Shoe.create(brand: "Sam Edelman", name: "Hilty Pointed Toe Block High-Heel Ankle Booties", color: "Black Suede", description: "Pointed toe, functional inside zip. 3 in. wrapped, bold block heel.",  img_url: "https://images.bloomingdalesassets.com/is/image/BLM/products/7/optimized/9985197_fpx.tif?$2014_BROWSE_FASHION$&hei=335&wid=268") 
-ws3 = Shoe.create(brand: "Alexander Wang", name: "Rina Studded Slingback Pumps", color: "Black", description: "Elasticized slingback strap; stud details at sole edge; pointed toe; 4.5 in wrapped stiletto heel.", img_url: "https://images.bloomingdalesassets.com/is/image/BLM/products/2/optimized/10116252_fpx.tif?op_sharpen=1&wid=500&fit=fit,1&$filtersm$&fmt=webp")	 
+# # mb4 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb5 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb6 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
 
-user1 = User.create(name: "Cindy", password: "abc")
-user2 = User.create(name: "Poppy", password: "pop")
+# # mb7 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb8 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb9 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+# # mb10 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb11 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# # mb12 = Bottom.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+# ms1 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms2 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms3 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+# ms4 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms5 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms6 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+# ms7 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms8 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms9 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+# ms10 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms11 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+# ms12 = Shoe.create(brand: "Nike", name: "", color: "", description: "mens", img_url: "")
+
+wt2 = Top.create(brand: "Nike", name: "Nike Infinite", color: "Black", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/vxe0biau3bc8slln8ioi/infinite-womens-long-sleeve-running-top-x7t3NW.jpg")
+wt3 = Top.create(brand: "Nike", name: "Nike Infinite", color: "White", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/jczv1lugsa7cvvelbgcj/infinite-womens-long-sleeve-running-top-x7t3NW.jpg")
+wt1 = Top.create(brand: "Nike", name: "Nike Yoga Luxe", color: "Grey", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7cb4c110-8595-4bb9-a04e-2cdba667a88a/yoga-luxe-womens-cropped-hoodie-NndxCg.jpg")
+
+# wt4 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt5 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt6 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# wt7 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt8 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt9 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# wt10 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt11 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wt12 = Top.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+wb1 = Bottom.create(brand: "Nike", name: "Nike Sportswear Tech Fleece", color: "Black", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cdufquim81tzc36r25hc/sportswear-tech-fleece-womens-pants-NQJsVt.jpg")
+wb2 = Bottom.create(brand: "Nike", name: "Nike Sportswear Tech Fleece", color: "Grey", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/rskhuxezabqqgjnizgvy/sportswear-tech-fleece-womens-pants-NQJsVt.jpg")
+wb3 = Bottom.create(brand: "Nike", name: "Nike Sportswear Tech Fleece", color: "Desert Dust", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/24a55c0c-a22e-4eda-99fa-653fdefa6155/sportswear-tech-fleece-womens-pants-NQJsVt.jpg")
+
+# wb4 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb5 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb6 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# wb7 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb8 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb9 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# wb10 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb11 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# wb12 = Bottom.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+ws1 = Shoe.create(brand: "Nike", name: "Nike Blazer Mid '77", color: "White", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7dcc6fd4-b41c-493e-85bd-58b8944b6b1d/blazer-mid-77-womens-shoe-25h71W.jpg")
+ws2 = Shoe.create(brand: "Nike", name: "Nike RYZ 365", color: "Black", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/xeaqubyuoha4tritk9tl/ryz-365-womens-shoe-LkNlGp.jpg")
+ws3 = Shoe.create(brand: "Nike", name: "Nike RYZ 365", color: "Rose", description: "womens", img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/jdqq7ybbdri9xvdnqw8y/ryz-365-womens-shoe-LkNlGp.jpg")
+
+# ws4 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws5 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws6 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# ws7 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws8 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws9 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+# ws10 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws11 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+# ws12 = Shoe.create(brand: "Nike", name: "", color: "", description: "womens", img_url: "")
+
+user1 = User.create(username: "Cindy", password: "abc")
+user2 = User.create(username: "Poppy", password: "pop")
 
 of1 = Outfit.create(top_id: 1, bottom_id: 3, shoe_id: 2, user_id: 1, likes: 2, name: "Casual")
 of2 = Outfit.create(top_id: 3, bottom_id: 4, shoe_id: 1, user_id: 2, likes: 3, name: "Dat Drip")
